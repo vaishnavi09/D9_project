97fd954e6f2586fd729b725562ab8fbfdbd23422
cmr <muralivaishnavi0924@gmail.com>
2017-05-06 14:16:29 +0530

    this is initial app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-#root to:"resumes#index"
+ root to:"resumes#index"
   resources :resumes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
